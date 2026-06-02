ea291004cbd903944d7c54f12e725af016c7576d
BSandrez <bsandrez@luxmodernis.com>
2026-06-02 10:12:04 +0200

    Add files via upload

--- api/portrait/[name].js
+++ api/portrait/[name].js
@@ -1,1 +1,16 @@
+import { list } from "@vercel/blob";
 
+export default async function handler(req, res) {
+  const { name } = req.query;
+  try {
+    const { blobs } = await list({ prefix: `portraits/${name}` });
+    if (!blobs.length) return res.status(404).end();
+    const r = await fetch(blobs[0].url);
+    const buffer = await r.arrayBuffer();
+    res.setHeader("Content-Type", "image/jpeg");
+    res.setHeader("Cache-Control", "public, max-age=86400");
+    res.send(Buffer.from(buffer));
+  } catch {
+    res.status(500).end();
+  }
+}
