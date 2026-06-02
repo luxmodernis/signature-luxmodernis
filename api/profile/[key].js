3b35ef6fade688b742d0dd59a0ddf948daaed58b
BSandrez <bsandrez@luxmodernis.com>
2026-06-02 10:12:41 +0200

    Add files via upload

--- api/profile/[key].js
+++ api/profile/[key].js
@@ -1,1 +1,14 @@
+import { list } from "@vercel/blob";
 
+export default async function handler(req, res) {
+  const { key } = req.query;
+  try {
+    const { blobs } = await list({ prefix: `profiles/${key}.json` });
+    if (!blobs.length) return res.json(null);
+    const r = await fetch(blobs[0].url + "?t=" + Date.now());
+    const data = await r.json();
+    res.json(data);
+  } catch {
+    res.json(null);
+  }
+}
