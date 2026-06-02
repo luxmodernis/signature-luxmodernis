7d74bcf7d93850b6bf3c1728a9abe82d1b0e185a
BSandrez <bsandrez@luxmodernis.com>
2026-06-02 11:03:08 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __BUILD_DATE__: JSON.stringify(
+      new Date().toLocaleString('fr-FR', {
+        day: '2-digit', month: '2-digit', year: 'numeric',
+        hour: '2-digit', minute: '2-digit'
+      })
+    )
+  }
 })
